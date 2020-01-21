9e36ecc9ca4ddff3e3a324ecdffd9c5c4d74cef8
Derrick Mungai <derrickmungai@gmail.com>
2020-01-21 20:54:05 +0000

    Chapter 3

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -25,7 +25,7 @@ type User {
 
 type Mission {
   name: String
-  missionPatch(size: PatchSize): String
+  missionPatch(mission: String, size: PatchSize): String
 }
 
 enum PatchSize {
@@ -33,9 +33,29 @@ enum PatchSize {
   LARGE
 }
 type Query {
-  launches: [Launch]!
+  launches( # replace the current launches query with this one.
+    """
+    The number of results to show. Must be >= 1. Default = 20
+    """
+    pageSize: Int
+    """
+    If you add a cursor here, it will only return results _after_ this cursor
+    """
+    after: String
+  ): LaunchConnection!
   launch(id: ID!): Launch
   me: User
+}
+
+"""
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+type LaunchConnection { # add this below the Query type as an additional type.
+  cursor: String!
+  hasMore: Boolean!
+  launches: [Launch]!
 }
 
 type Mutation {
@@ -49,6 +69,7 @@ type TripUpdateResponse {
   message: String
   launches: [Launch]
 }
+
 `;
 
 module.exports = typeDefs;
